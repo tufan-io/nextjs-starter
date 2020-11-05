82805c51df55d171f484e55e0d1b3921ac9827d9
Shishir <sramam@users.noreply.github.com>
2020-11-05 10:49:09 -0800

    fix: adding jest, improving readme

--- README.md
+++ README.md
@@ -6,4 +6,21 @@ https://paulintrognon.fr/blog/post/typescript-prettier-eslint-next-js
 Following those steps is recommended at least one time.
 This repo is meant as a starter for the 2nd+ time...
 
+
 ## Getting started
+
+1. Hit the green ["Use this template"](https://github.com/tufan-io/nextjs-starter/generate) button on the [repo](https://github.com/tufan-io/nextjs-starter)
+
+2. `git clone [URL of the generated template]`
+3. `cd [repo-dir]`
+4. `npm init`
+5. `npm install`
+
+Get cracking on the new project that you want to build...
+
+
+### Helpful resources
+
+- [nextjs example blog app - TypeScript implementation](https://github.com/vercel/next-learn-starter/tree/master/typescript-final) is the 
+app used in the [nextjs typescript tutorial](https://nextjs.org/learn/excel/typescript). It demonstrates many of the specifics of
+using nextjs.
--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     roots: ['<rootDir>'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
-    testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
+    testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next|coverage|.vscode)[/\\\\]'],
     transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
     transform: {
         '^.+\\.(ts|tsx)$': 'babel-jest',
@@ -12,4 +12,27 @@ module.exports = {
         '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
         '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
     },
+    coverageDirectory: "coverage",
+    collectCoverageFrom: [
+        '<rootDir>/**/*.{ts, tsx}',
+        //     "lib/**",
+        //     "pages/**",
+        //     "public/**",
+        //     "posts/**",
+        //     "styles/**",
+    ],
+    // roots: ['<rootDir>'],
+    // testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|tsx)$',
+    // coveragePathIgnorePatterns: [
+    //     "/node_modules",
+    //     "**/test.ts",
+    //     "**/test.tsx",
+    //     "**/test/*",
+    // ],
+    collectCoverage: true,
+    coverageReporters: [
+        "lcov",
+        "text-summary",
+        "json-summary"
+    ]
 }
